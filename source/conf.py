84215b686e3a9ecd07735eebcf8c8f07cd2c51a7
Fred-Evif <71593073+Fred-Evif@users.noreply.github.com>
2024-04-23 11:54:29 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -47,7 +47,6 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.extlinks",
     "sphinx.ext.autosummary",
-    "hoverxref.extension",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
